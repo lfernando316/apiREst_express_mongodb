7bcbd00dab70c3de67f463f8c3597ffc933bb757
lfernando316 <luiz_fernando316@hotmail.com>
2023-09-14 23:10:59 -0500

    creacion de middleware

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,4 @@
-import { validationResult } from "express-validator";
 export const register = (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
   console.log(req.body);
   res.json({ ok: true });
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { login, register } from "../controllers/auth.controller.js";
 import { body } from "express-validator";
+import { validationResultExpress } from "../middlewares/validationResultExpress.js";
 const router = express.Router();
 
 router.post(
@@ -10,18 +11,30 @@ router.post(
       .trim()
       .isEmail()
       .normalizeEmail(),
-    body("password", "formato de password incorrecta")
-      .trim()
-      .isLength({ min: 6 })
-      .custom((value, { req }) => {
+    body("password", "minimo 6 caracteres").trim().isLength({ min: 6 }),
+    body("password", "formato de password incorrecta").custom(
+      (value, { req }) => {
         if (value !== req.body.repassword) {
           throw new Error("no coinciden las contraseñas");
         }
         return value;
-      }),
+      }
+    ),
   ],
+  validationResultExpress,
   register
 );
-router.post("/login", login);
+router.post(
+  "/login",
+  [
+    body("email", "formato de email incorrecto")
+      .trim()
+      .isEmail()
+      .normalizeEmail(),
+    body("password", "minimo 6 caracteres").trim().isLength({ min: 6 }),
+  ],
+  validationResultExpress,
+  login
+);
 
 export default router;
