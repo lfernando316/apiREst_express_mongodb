c3d7c3fced7cbae146311a59fba340d1fb855dc2
lfernando316 <luiz_fernando316@hotmail.com>
2023-09-24 08:32:11 -0500

    inplementacion de refreshToken

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/User.js";
 import jwt from "jsonwebtoken";
+import { generateRefreshToken, generateToken } from "../utils/tokenManager.js";
 
 export const register = async (req, res) => {
   const { email, password } = req.body;
@@ -39,11 +40,46 @@ export const login = async (req, res) => {
         .json({ error: "Contraseña incorrecta o correo invalido" });
 
     // Generar el token JWT
-    const token = jwt.sign({ uid: user._id }, process.env.JWT_SECRET);
+    const { token, expiresIn } = generateToken(user.id);
+    generateRefreshToken(user.id, res);
 
-    return res.json({ ok: token });
+    return res.json({ token, expiresIn });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "Error de servidor" });
   }
+};
+
+export const infoUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.uid).lean();
+    return res.json({ email: user.email, uid: user.id });
+  } catch (error) {
+    return res.status(500).json({ error: "error de server" });
+  }
+};
+
+export const refreshToken = (req, res) => {
+  try {
+    const refreshTokenCookie = req.cookies.refreshToken;
+    if (!token) throw new Error("no existe el token");
+
+    const { uid } = jwt.verify(refreshTokenCookie, process.env.JWT_REFRESH);
+    const { token, expiresIn } = generateToken(uid);
+
+    return res.json({ token, expiresIn });
+  } catch (error) {
+    console.log(error);
+    const TokenVerificationErrors = {
+      "invalid signature": "la firma del jwt no es valida",
+    };
+    return res
+      .status(401)
+      .send({ error: TokenVerificationErrors[error.message] });
+  }
+};
+
+export const logout = (req, res) => {
+  res.clearCookie("refreshToken");
+  res.json({ ok: true });
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,7 +1,14 @@
 import { Router } from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import {
+  infoUser,
+  login,
+  register,
+  refreshToken,
+  logout,
+} from "../controllers/auth.controller.js";
 import { body } from "express-validator";
 import { validationResultExpress } from "../middlewares/validationResultExpress.js";
+import { requireToken } from "../middlewares/requireToken.js";
 const router = Router();
 
 router.post(
@@ -36,5 +43,18 @@ router.post(
   validationResultExpress,
   login
 );
+// http://localhost:500/api/v1/auth/login
+// {
+//   "email": "lfernando316@hotmail.com",
+//   "password": "123123",
+//   "repassword": "123123"
+// }
+
+router.get("/protected", requireToken, infoUser);
+// http://localhost:500/api/v1/auth/protected
+
+router.get("/refresh", refreshToken);
+
+router.get("/logout", logout);
 
 export default router;
