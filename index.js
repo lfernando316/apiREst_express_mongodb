93b3df98ea60a8208127c499a2fe772a1f7d401f
lfernando316 <luiz_fernando316@hotmail.com>
2023-10-01 18:52:21 -0500

    deploy

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const whiteList = [process.env.ORIGIN1];
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (whiteList.includes(origin)) {
+      if (!origin || whiteList.includes(origin)) {
         return callback(null, origin);
       }
       return callback("Error de CORS origin: " + origin + "no autorizado!");
